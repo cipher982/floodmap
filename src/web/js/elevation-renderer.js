85a848715c9a74d9a6ae8720644375ed7ba2533f
David Rose <david@drose.io>
2025-12-12 16:29:30 -0500

    Chore: bump tile cache key to pick regenerated low-zoom tiles

--- src/web/js/elevation-renderer.js
+++ src/web/js/elevation-renderer.js
@@ -85,7 +85,7 @@ class ElevationRenderer {
         const qs = new URLSearchParams();
         qs.set('method', 'precompressed');
         if (window.location.hostname === 'localhost') qs.set('t', String(Date.now()));
-        qs.set('v', '20251212f');
+        qs.set('v', '20251212g');
         const url = `/floodmap/api/v1/tiles/elevation-data/${z}/${x}/${y}.u16?${qs.toString()}`;
 
         const loadPromise = fetch(url, { signal })
